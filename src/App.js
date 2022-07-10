1ec5b31146df018cce9e472767925d585f690dc0
webmasters2019 <lively222@live.com>
2022-07-11 02:48:34 +0300

    working on token and protected routes

--- src/App.js
+++ src/App.js
@@ -4,35 +4,22 @@ import Account from './components/Account';
 import FreeComponent from './components/FreeComponent';
 import ProtectedComponent from './components/ProtectedComponent';
 import ProtectedRoutes from './components/ProtectedRoutes';
+import { AuthProvider } from './components/AuthProvider';
 
 function App() {
   return (
     <Container>
-      <Row>
-        <Col className='text-center'>
-          <h1>React Authentication Tutorial</h1>
-          <section id='navigation'>
-            <a href='/'>Home</a>
-            <a href='/free'>Free Component</a>
-            <a href='/protected'>Protected Component</a>
-          </section>
-          </Col>
-          </Row>
-          <Row>
-            <Account />
-          </Row>
-          {/* create routes here */}
-        <Routes>
-          <Route exact path='/' component={Account} />
-          <Route exact path='/free' component={FreeComponent} />
-          <Route path='/protected' element={
-            <ProtectedRoutes>
-              <ProtectedComponent />
-            </ProtectedRoutes>
-          }
-          />
-        </Routes>
-
+     <Routes>
+      <Route path='/' element={<Account />} />
+      <Route path='/freecomponent' element={<FreeComponent />} />
+      <Route path='/protectedcomponent'
+      element={
+      <ProtectedRoutes>
+        <ProtectedComponent />
+      </ProtectedRoutes>
+      } 
+      />
+     </Routes>
     </Container>
   );
 }
